8a5ace58c2713c493f9409b5602d41ff3d0c1dbe
Pilybarbosa <pilycarolinabarbosalemus@gmail.com>
2023-02-17 12:14:00 -0500

    nota

---
+++ src/Nota.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Nota {
+    public static void main(String[] args) {
+        Double nota1,nota2, nota3, asistencia, promedio;
+        String nombre;
+        Scanner lectura=new Scanner (System.in);
+
+        System.out.println("nombre");
+        nombre=lectura.next();
+        System.out.println(" ingrese su primera nota ");
+        nota1=lectura.nextDouble();
+        System.out.println(" ingrese su segunda nota ");
+        nota2=lectura.nextDouble();
+        System.out.println(" ingrese su tercera nota");
+        nota3=lectura.nextDouble();
+        System.out.println(" ingrese su promedio de asistencia ");
+        asistencia=lectura.nextDouble();
+
+        promedio=(nota1+nota2+nota3+asistencia)/3;
+
+        if (promedio>=3.5 && asistencia>=70 || nombre.equals("jennifer")){
+            //si condicion se cumple 
+            System.out.println("aprobaste "); 
+
+        } else { 
+            // si condicion no se cumple 
+            System.out.println(" no aprobaste ");
+            
+        }
+        lectura.close();
+    }
+}
---
+++ src/Password.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class password {
+     public static void main(String[] args) {
+        Scanner.lectura=new Scanner (System.in);
+        
+     }
+}
